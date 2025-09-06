f9f5041ad11537d55525d59f7c0a2e19c6e12c75
GeonWoo Im <gunwoo14789@naver.com>
2025-09-06 15:08:03 +0900

    pcce 기출문제 5번 심폐소생술

---
+++ PCCE 기출문제 5번  심폐소생술.py
@@ -0,0 +1,11 @@
+def solution(cpr):
+    answer = []
+    basic_order = ["check", "call", "pressure", "respiration", "repeat"]
+    for action in cpr:
+        for i in range(5):
+            if action == basic_order[i]:
+                answer.append(i+1)
+    return answer
+
+cpr = ["call", "respiration", "repeat", "check", "pressure"]
+print(solution(cpr))
