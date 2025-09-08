9775340bd975e50b06df8214b87abbecd5dd08ae
GeonWoo Im <gunwoo14789@naver.com>
2025-09-08 12:42:19 +0900

    대소문자 바꿔서 출력

--- 대소문자 바꿔서 출력.py
+++ 대소문자 바꿔서 출력.py
@@ -7,3 +7,7 @@ for i in str:
         answer += i.upper()
 print(answer)        
         
+#대문자인지 판별: 변수.isupper()
+#소문자인지 판별: 변수.islower()
+#소문자 -> 대문자 변환: 변수.upper()
+#대문자 -> 소문자 변환: 변수.lower()         
