7f2c7b1ecd970d6b634786c58440e18571c732d5
GeonWoo Im <gunwoo14789@naver.com>
2025-09-11 12:52:21 +0900

    코드 처리하기

---
+++ 코드 처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    ret = ""
+    mode = 0
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] != '1':
+                if i % 2 == 0:
+                    ret += code[i]
+            else:
+                mode = 1
+        else:  
+            if code[i] != '1':
+                if i % 2 == 1:
+                    ret += code[i]
+            else:
+                mode = 0
+    return "EMPTY" if not ret else ret
